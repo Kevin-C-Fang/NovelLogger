d7ca1f409726e4149205c137e498fadd42dc2d00
kfang749@gmail.com <kfang749@gmail.com>
2026-01-02 13:17:34 -0500

    Added EF core nuget packages and ApplicationDbContext

--- NovelLogger/Program.cs
+++ NovelLogger/Program.cs
@@ -1,7 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using NovelLogger.Data;
+using Microsoft.AspNetCore.Identity;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
 
 var app = builder.Build();
 
