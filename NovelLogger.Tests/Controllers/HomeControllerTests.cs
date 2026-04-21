c4ce95ea51f2defba392f94cf070be178c946a67
kfang749@gmail.com <kfang749@gmail.com>
2026-04-21 18:30:23 -0400

    Added xUnit test project and completed tests for HomeController

---
+++ NovelLogger.Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,143 @@
+﻿using Castle.Core.Logging;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NovelLogger.Controllers;
+using NovelLogger.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NovelLogger.Tests.Controllers
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void Index_ReturnsRedirectToBookmark_WhenUserIsAuthenticated()
+        {
+            var loggerMock = new Mock<ILogger<HomeController>>();
+            var controller = new HomeController(loggerMock.Object);
+
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.Name, "test")
+                },
+                authenticationType: "TestAuth"));
+
+            controller.ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext
+                {
+                    User = user
+                }
+            };
+
+            var result = controller.Index();
+            var redirect = Assert.IsType<RedirectToActionResult>(result);
+            Assert.Equal("Index", redirect.ActionName);
+            Assert.Equal("Bookmark", redirect.ControllerName);
+        }
+
+        [Fact]
+        public void Index_ReturnsView_WhenUserIsNotAuthenticated()
+        {
+            var loggerMock = new Mock<ILogger<HomeController>>();
+            var controller = new HomeController(loggerMock.Object);
+            var user = new ClaimsPrincipal(new ClaimsIdentity());
+
+            controller.ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext
+                {
+                    User = user
+                }
+            };
+
+            var result = controller.Index();
+            Assert.IsType<ViewResult>(result);
+        }
+
+        [Fact]
+        public void Privacy_ReturnsView_WhenRoutedTo()
+        {
+            var loggerMock = new Mock<ILogger<HomeController>>();
+            var controller = new HomeController(loggerMock.Object);
+
+            var result = controller.Index();
+            Assert.IsType<ViewResult>(result);
+        }
+
+        [Fact]
+        public void Error_WhenExceptionFeatureExists_LogsErrorAndReturnsViewModel()
+        {
+            var loggerMock = new Mock<ILogger<HomeController>>();
+            var controller = new HomeController(loggerMock.Object);
+            var exception = new Exception();
+            var exceptionFeature = new ExceptionHandlerFeature
+            {
+                Error = exception,
+                Path = "/Home/Error"
+            };
+
+            var httpContext = new DefaultHttpContext();
+            httpContext.TraceIdentifier = "TestTrace-123";
+            httpContext.Features.Set<IExceptionHandlerPathFeature>(exceptionFeature);
+
+            controller.ControllerContext = new ControllerContext
+            {
+                HttpContext = httpContext
+            };
+
+            var result = controller.Error();
+
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var model = Assert.IsType<ErrorViewModel>(viewResult.Model);
+            Assert.Equal(httpContext.TraceIdentifier, model.RequestId);
+
+            loggerMock.Verify(
+                x => x.Log(
+                    LogLevel.Error,
+                    It.IsAny<EventId>(),
+                    It.Is<It.IsAnyType>((v, t) =>
+                        v.ToString()!.Contains("Unhandled exception at")),
+                    exception,
+                    It.IsAny<Func<It.IsAnyType, Exception?, string>>()),
+                Times.Once);
+        }
+
+        [Fact]
+        public void Error_WhenExceptionFeatureMissing_ReturnsViewModelAndDoesNotLog()
+        {
+            var loggerMock = new Mock<ILogger<HomeController>>();
+            var controller = new HomeController(loggerMock.Object);
+            var httpContext = new DefaultHttpContext();
+            httpContext.TraceIdentifier = "TestTrace-123";
+
+            controller.ControllerContext = new ControllerContext
+            {
+                HttpContext = httpContext
+            };
+
+            var result = controller.Error();
+
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var model = Assert.IsType<ErrorViewModel>(viewResult.Model);
+            Assert.Equal(httpContext.TraceIdentifier, model.RequestId);
+
+            loggerMock.Verify(
+                x => x.Log(
+                    LogLevel.Error,
+                    It.IsAny<EventId>(),
+                    It.IsAny<It.IsAnyType>(),
+                    It.IsAny<Exception>(),
+                    It.IsAny<Func<It.IsAnyType, Exception?, string>>()),
+                Times.Never);
+        }
+    }
+}
